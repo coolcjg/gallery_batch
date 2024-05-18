3636815f30a339fc4a931cdf5f02432b69f8f712
coolcjg <coolcjg@naver.com>
2024-05-18 12:24:22 +0900

    초기화

---
+++ src/main/kotlin/com/cjg/batch/GalleryBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.cjg.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GalleryBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<GalleryBatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/cjg/batch/GalleryBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cjg.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GalleryBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
