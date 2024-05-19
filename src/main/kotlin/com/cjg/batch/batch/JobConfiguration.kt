8fcb3d3281db05158ef3205fa08a8072a91ac153
coolcjg <coolcjg@naver.com>
2024-05-19 16:55:52 +0900

    스케줄러에 JOB등록

--- src/main/kotlin/com/cjg/batch/batch/JobConfiguration.kt
+++ src/main/kotlin/com/cjg/batch/batch/JobConfiguration.kt
@@ -4,9 +4,13 @@ import com.cjg.batch.dto.GalleryDto
 import com.cjg.batch.service.GalleryService
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.item.support.ListItemReader
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -18,22 +22,30 @@ class JobConfiguration(
 
     @Bean
     fun job(jobRepository: JobRepository, transactionManager:PlatformTransactionManager) : Job {
-        return JobBuilder("testJob19", jobRepository)
-            .start(step(jobRepository, transactionManager))
-            .build()
+        println("Job 빈 생성")
+    return JobBuilder("BatchJob", jobRepository)
+        .start(step(jobRepository, transactionManager))
+        .incrementer(RunIdIncrementer())
+        .build()
     }
 
     @Bean
+    @JobScope
     fun step(jobRepository: JobRepository, platformTransactionManager: PlatformTransactionManager,
              ): Step {
 
-                val list = galleryService.list()
-
-                return StepBuilder("collectStep", jobRepository)
-                    .chunk<GalleryDto, String>(3, platformTransactionManager)
-                    .reader(GalleryReader(list))
+                return StepBuilder("Step", jobRepository)
+                    .chunk<GalleryDto, String>(2, platformTransactionManager)
+                    .reader(reader())
                     .processor(GalleryProcessor())
                     .writer(GalleryWriter())
+                    .allowStartIfComplete(true)
                     .build()
+    }
+
+    @Bean
+    @StepScope
+    fun reader() : ListItemReader<GalleryDto> {
+        return ListItemReader<GalleryDto>(galleryService.list())
     }
 }
--- src/main/kotlin/com/cjg/batch/batch/Schedule.kt
+++ src/main/kotlin/com/cjg/batch/batch/Schedule.kt
@@ -1,13 +1,22 @@
 package com.cjg.batch.batch
 
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class Schedule {
+class Schedule(
+    val jobLauncher: JobLauncher,
+    val job : Job
+){
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(cron = "0/10 * * * * *")
     fun scheduleTaskUsingCronExpression(){
+
+        val jobParameter = JobParametersBuilder().toJobParameters();
+        jobLauncher.run(job, jobParameter)
 
     }
 }
